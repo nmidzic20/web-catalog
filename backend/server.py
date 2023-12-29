a6dd6ea455b28c8291ecf85f23368be03824bcb2
Dario Ljubas <darioljubas33@gmail.com>
2023-12-29 08:43:01 +0100

    Added check for port usage and skipping to next port if taken

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 FRONTEND_DIR = "../frontend"
 
@@ -23,26 +24,41 @@ def request_handler(request):
 
 def main():
     SERVER_HOST = "127.0.0.1"
-    SERVER_PORT = 8000
+    server_port = 8000
+
+    connection_exception = ""
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((SERVER_HOST, SERVER_PORT))
+    
+    while connection_exception != None:
+        try:
+            server_socket.bind((SERVER_HOST, server_port))
+            connection_exception = None
+        except Exception as exc:
+            connection_exception = exc
+            
+            if "[Errno 98] Address already in use" in str(exc):
+                print(f"Port {server_port} already taken.")
+                server_port += 1
+                print(f"Trying port {server_port}...")
+        time.sleep(1)
+            
     server_socket.listen()
 
-    print(f"Server is listening on IP address and port: {SERVER_HOST}:{SERVER_PORT}")
+    print(f"Server is listening on IP address and port: {SERVER_HOST}:{server_port}")
 
     while True:
         try:
-            cient_socket, client_address = server_socket.accept()
+            client_socket, client_address = server_socket.accept()
             print(f"Connected to: {client_address}")
 
-            request = cient_socket.recv(4096).decode("utf-8") # mozda maknuti utf-8 zbog slika
+            request = client_socket.recv(4096).decode("utf-8") # mozda maknuti utf-8 zbog slika
             print(request)
 
             response = request_handler(request)
-            cient_socket.sendall(response.encode()) # mozda ce olaksati slanje slika
+            client_socket.sendall(response.encode()) # mozda ce olaksati slanje slika
 
-            cient_socket.close()
+            client_socket.close()
 
         except Exception as exc:
             print(exc)
