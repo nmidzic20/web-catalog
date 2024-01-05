438e26159ccc8436474d28685c3891f8de7e8b0b
StjepanPetrovic <stjepanpetrovic11@gmail.com>
2024-01-05 16:20:46 +0100

    Add methods for creating a new grocery and for getting all groceries

--- backend/groceries.py
+++ backend/groceries.py
@@ -1,3 +1,6 @@
+from database import DB
+
+
 class Grocery:
     id: int
     name: str
@@ -8,3 +11,29 @@ class Grocery:
         self.name = name
         self.carbs = carbs
         self.picture = picture
+
+
+class GroceryHandler:
+    def get_all_groceries(self):
+        db = DB.get_instance('../web-catalog.sqlite')
+        db.connect_to_database()
+
+        try:
+            query_result = db.execute_query("SELECT * FROM Grocery")
+            print(query_result)
+            return query_result
+        finally:
+            db.close_connection()
+
+    def create_grocery(self, grocery: Grocery):
+        db = DB.get_instance('../web-catalog.sqlite')
+        db.connect_to_database()
+
+        try:
+            query = f"""
+                INSERT INTO Grocery(name, carbs, picture)
+                VALUES('{grocery.name}', '{grocery.carbs}', '{grocery.picture}');
+            """
+            db.execute_run_query(query)
+        finally:
+            db.close_connection()
