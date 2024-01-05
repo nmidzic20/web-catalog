2ba90c00502d4e41b138303c6667fa4b3ee9c3db
Noa Midzic <nmidzic@yahoo.com>
2024-01-05 11:49:30 +0100

    Prepare display of groceries on frontend

--- frontend/scripts/api.js
+++ frontend/scripts/api.js
@@ -2,7 +2,7 @@ var baseUrl = "http://localhost:8000";
 var urlRecipes = baseUrl + "/recipes";
 var urlGroceries = baseUrl + "/groceries";
 
-var baseApiUrl = "http://localhost:8000/api";
+var baseApiUrl = baseUrl + "/api";
 var apiRecipes = baseApiUrl + "/recipes";
 var apiGroceries = baseApiUrl + "/groceries";
 
--- frontend/scripts/ui/init/grocery/load-grocery-catalog.js
+++ frontend/scripts/ui/init/grocery/load-grocery-catalog.js
@@ -1,12 +1,32 @@
 function insertTempGroceryGrid() {
-    var grid = document.getElementById('groceries');
-    if (db.groceryItems.length === 0) {
-        setVisibility('no-groceries', true);
-        return;
-    } else {
-        setVisibility('no-groceries', false);
-        grid.innerHTML = db.groceryItems.map(function(grocery) {
-            return grocery.getClickableHtmlDisplay()
-        }).join('');
-    }
+  var grid = document.getElementById("groceries");
+  if (db.groceryItems.length === 0) {
+    setVisibility("no-groceries", true);
+    return;
+  } else {
+    setVisibility("no-groceries", false);
+    grid.innerHTML = db.groceryItems
+      .map(function (grocery) {
+        return grocery.getClickableHtmlDisplay();
+      })
+      .join("");
+  }
 }
+
+function insertGroceriesIntoGrid() {
+  const grid = document.getElementById("groceries");
+
+  fetchGroceries().then((groceries) => {
+    if (groceries.length === 0) {
+      setVisibility("no-groceries", true);
+    } else {
+      setVisibility("no-groceries", false);
+      grid.innerHTML = groceries
+        .map((grocery) => grocery.getClickableHtmlDisplay())
+        .join("");
+    }
+  });
+}
+
+// pozvati kad je implementiran endpoint na backendu
+// insertGroceriesIntoGrid();
