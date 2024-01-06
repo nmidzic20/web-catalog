7e0fc4c2c58a7375bc1ff7192865cbbee2cdbac7
Noa Midzic <nmidzic@yahoo.com>
2024-01-06 12:03:11 +0100

    Implement color indicator for active nav link for current page

--- frontend/scripts/helpers/helper-functions.js
+++ frontend/scripts/helpers/helper-functions.js
@@ -1,17 +1,38 @@
 function changeVisibility(id) {
-    var element = document.getElementById(id);
-    if (element.classList.contains('hidden')) {
-        element.classList.remove('hidden');
-    } else {
-        element.classList.add('hidden');
-    }
+  var element = document.getElementById(id);
+  if (element.classList.contains("hidden")) {
+    element.classList.remove("hidden");
+  } else {
+    element.classList.add("hidden");
+  }
 }
 
 function setVisibility(id, visible) {
-    var element = document.getElementById(id);
-    if (visible) {
-        element.classList.remove('hidden');
-    } else {
-        element.classList.add('hidden');
-    }
+  var element = document.getElementById(id);
+  if (visible) {
+    element.classList.remove("hidden");
+  } else {
+    element.classList.add("hidden");
+  }
+}
+
+function setActive(clickedElement) {
+  var navLinks = document.getElementsByClassName("nav-link");
+  for (var i = 0; i < navLinks.length; i++) {
+    navLinks[i].classList.remove("active");
+  }
+
+  clickedElement.classList.add("active");
+}
+
+function setActiveLink() {
+  var currentUrl = window.location.href;
+
+  if (currentUrl.includes("recipes")) {
+    setActive(document.querySelector('a[href="/recipes"]'));
+  }
+
+  if (currentUrl.includes("groceries")) {
+    setActive(document.querySelector('a[href="/groceries"]'));
+  }
 }
